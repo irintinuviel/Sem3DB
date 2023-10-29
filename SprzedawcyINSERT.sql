7e9e065f343f64d6ac9defbb45166aa177565d08
irintinuviel <sara.rypinska@op.pl>
2023-10-29 17:30:14 +0100

    uzupelniono nowe tabele#

---
+++ SprzedawcyINSERT.sql
@@ -0,0 +1,32 @@
+INSERT INTO Sprzedawcy (Imie, Nazwisko, NrPrzelozonego) VALUES
+('Józef', 'Kormoran', NULL),
+('Monika', 'Wilk', NULL),
+('Dariusz', 'Dereń', NULL),
+('Anita', 'Nitkowska', 1),
+('Marian', 'Wodzirej', 1),
+('Ewelina', 'Dąbrowska',2),
+('Paweł', 'Lewandowski',2),
+('Katarzyna', 'Wójcik',1),
+('Tomasz', 'Kamiński',2),
+('Agnieszka', 'Kowalczyk', 3),
+('Marcin', 'Zieliński', 3),
+('Magdalena', 'Szymańska', 1),
+('Paweł', 'Woźniak', 2),
+('Adam', 'Jankowski', 3),
+('Justyna', 'Wojciechowska', 3),
+('Krzysztof', 'Moroń', 3),
+('Natalia', 'Kaczmarek', 3),
+('Michał', 'Mazur', 1),
+('Karolina', 'Krawczyk', 2),
+('Andrzej', 'Piotrowski', 2),
+('Aleksandra', 'Grabowska', 3),
+('Łukasz', 'Pawłowski', 3),
+('Izabela', 'Michalska', 3),
+('Roman', 'Nowakowski', 2),
+('Paulina', 'Adriańczyk', 1);
+
+
+
+
+
+
